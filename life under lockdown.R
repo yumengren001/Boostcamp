3155b3695cc1e0210ae514b7a2cb6d9732b9f4da
yumengren001 <k21023991@kcl.ac.uk>
2022-01-23 14:39:22 +0000

    newest

--- life under lockdown.R
+++ life under lockdown.R
@@ -1,4 +1,9 @@
 library(dplyr)
 library(readr)
 lul2_d <- read_csv("D:/R-PROJECT/life-under-lockdown/life-under-lockdown-wave2-extract.csv")
-
+lul2_d %>% mutate(know_facebook = 
+                    ordered(know_facebook, 
+                    levels = c("Nothing at all","Not very much","A fair amount","A great deal")
+                    )) %>% 
+          group_by(know_facebook) %>%
+          summarise(n=n(), beliefs= mean(beliefs))
